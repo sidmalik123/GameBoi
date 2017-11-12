1ef2e1a5f315f5b5976ae1055ce1b1cdb2f0c952
Siddharth Malik <siddharthmalik123@gmail.com>
2017-11-11 23:10:10 -0500

    added immediate and address value types in load

--- src/main/java/cpu/GBCPU.java
+++ src/main/java/cpu/GBCPU.java
@@ -4,6 +4,8 @@ import mmu.MMU;
 
 public class GBCPU extends AbstractCPU {
 
+    private enum ValueType {IMMEDIATE, ADDRESS};
+
     private MMU mmu;
 
     private GBRegisterManager registerManager;
@@ -25,42 +27,46 @@ public class GBCPU extends AbstractCPU {
         switch (instruction & 0xFF) {
             // Loads
             case 0x06:
-                load(SingleRegister.B);
+                load(SingleRegister.B, ValueType.IMMEDIATE);
             case 0x0E:
-                load(SingleRegister.C);
+                load(SingleRegister.C, ValueType.IMMEDIATE);
             case 0x16:
-                load(SingleRegister.E);
+                load(SingleRegister.E, ValueType.IMMEDIATE);
             case 0x1E:
-                load(SingleRegister.E);
+                load(SingleRegister.E, ValueType.IMMEDIATE);
             case 0x26:
-                load(SingleRegister.H);
+                load(SingleRegister.H, ValueType.IMMEDIATE);
             case 0x2E:
-                load(SingleRegister.L);
-
-            // LDrrs
+                load(SingleRegister.L, ValueType.IMMEDIATE);
             case 0x7F:
 
         }
 
     }
 
-    // Load n(immediate value) into r
-//    private void LDrn(SingleRegister singleRegister) {
-//        registerManager.set(singleRegister, mmu.readData(++PC));
-//        numCyclesPassed += 8;
-//    }
-//
-//    // r1 = r2
-//    private void LDr1r2(SingleRegister singleRegister1, SingleRegister singleRegister2) {
-//        registerManager.set(singleRegister1, registerManager.get(singleRegister2));
-//        numCyclesPassed += 4;
-//    }
+    /*
+    *
+    * LOAD METHODS
+    *
+    * */
 
+    // loads immediate/address value into register
+    private void load(SingleRegister r1, ValueType valueType) {
+        switch (valueType) {
+            case IMMEDIATE:
+                registerManager.set(r1, mmu.readData(++PC));
+                numCyclesPassed += 8;
+            case ADDRESS:
+                registerManager.set(r1, getImmediateAddressValue());
+                numCyclesPassed += 16;
+        }
 
-    // loads immediate value into register
-    private void load(SingleRegister singleRegister) {
-        registerManager.set(singleRegister, mmu.readData(++PC));
-        numCyclesPassed += 8;
+    }
+
+    // loads the value in r1 to immediate address val
+    private void load(SingleRegister r1) {
+        mmu.writeData(getImmediateAddressValue(), registerManager.get(r1));
+        numCyclesPassed += 16;
     }
 
     // r2 = r1
@@ -69,7 +75,29 @@ public class GBCPU extends AbstractCPU {
         numCyclesPassed += 4;
     }
 
+    // r1 = high bits of r2
     private void load(SingleRegister r1, DoubleRegister r2) {
+        registerManager.set(r1, registerManager.getHigh(r2));
+        numCyclesPassed += 8;
+    }
 
+    // r1.high = r2
+    private void load(DoubleRegister r1, SingleRegister r2) {
+        registerManager.set(r1, registerManager.get(r2));
+        numCyclesPassed += 8;
+    }
+
+    // r1.high = immediate value
+    private void load(DoubleRegister r1) {
+        registerManager.set(r1, mmu.readData(++PC));
+        numCyclesPassed += 12;
+    }
+
+    // special loads
+
+    private int getImmediateAddressValue() {
+        int add1 = mmu.readData(++PC);
+        int add2 = mmu.readData(++PC);
+        return add2 << 8 | add1;
     }
 }
