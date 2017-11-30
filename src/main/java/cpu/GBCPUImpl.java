028c32848fcc969c7f18af13a225e7418eb34527
Siddharth Malik <siddharthmalik123@gmail.com>
2017-11-29 23:16:50 -0500

    made separate methods to push and pop stack

--- src/main/java/cpu/GBCPUImpl.java
+++ src/main/java/cpu/GBCPUImpl.java
@@ -1,7 +1,6 @@
 package cpu;
 
 import core.AbstractTimingSubject;
-import core.BitUtils;
 import cpu.interrupts.GBInterruptManager;
 import mmu.GBMMU;
 
@@ -16,9 +15,7 @@ public class GBCPUImpl extends AbstractTimingSubject implements GBCPU {
 
     private GBInterruptManager interruptManager;
 
-    private int PC; // set by the mmu on program load
-
-    private int numCyclesPassed;
+    private int programCounter; // set by the mmu on program load
 
     public void run(String programLocation) {
         // Todo - process interrupts after every instruction
@@ -29,7 +26,7 @@ public class GBCPUImpl extends AbstractTimingSubject implements GBCPU {
     }
 
     private int readInstruction() {
-        return mmu.readData(PC);
+        return mmu.readData(programCounter);
     }
 
     /**
@@ -37,7 +34,7 @@ public class GBCPUImpl extends AbstractTimingSubject implements GBCPU {
      *
      * @return num of clock cycles taken to execute the instruction
      * */
-    private int executeInstruction(int instruction) { // Note - update PC at the end
+    private int executeInstruction(int instruction) { // Note - update programCounter at the end
 
         switch (instruction & 0xFF) {
             case 0x06: return loadImmediateByteIntoRegister(SingleRegister.B);
@@ -96,6 +93,7 @@ public class GBCPUImpl extends AbstractTimingSubject implements GBCPU {
             case 0x5F: return loadRegisterFromRegister(SingleRegister.E, SingleRegister.A);
             case 0x67: return loadRegisterFromRegister(SingleRegister.H, SingleRegister.A);
             case 0x6F: return loadRegisterFromRegister(SingleRegister.L, SingleRegister.A);
+            case 0xF9: return loadRegisterFromRegister(DoubleRegister.SP, DoubleRegister.HL);
 
             case 0x70: return writeRegisterToMemory(DoubleRegister.HL, SingleRegister.B);
             case 0x71: return writeRegisterToMemory(DoubleRegister.HL, SingleRegister.C);
@@ -129,7 +127,13 @@ public class GBCPUImpl extends AbstractTimingSubject implements GBCPU {
             case 0x11: return loadImmediateWordIntoRegister(DoubleRegister.DE);
             case 0x21: return loadImmediateWordIntoRegister(DoubleRegister.HL);
             case 0x31: return loadImmediateWordIntoRegister(DoubleRegister.SP);
-            case 0xF9: return loadRegisterFromRegister(DoubleRegister.SP, DoubleRegister.HL);
+
+            case 0xF5: return pushRegisterToStack(DoubleRegister.AF);
+            case 0xC5: return pushRegisterToStack(DoubleRegister.BC);
+            case 0xD5: return pushRegisterToStack(DoubleRegister.DE);
+            case 0xE5: return pushRegisterToStack(DoubleRegister.HL);
+
+
 
 
 
@@ -149,7 +153,7 @@ public class GBCPUImpl extends AbstractTimingSubject implements GBCPU {
 
     /**
      * Loads immediate byte into register r,
-     * increments the PC by 1
+     * increments the programCounter by 1
      *
      * @return num of cpu cycles taken to perform op
      * */
@@ -248,412 +252,63 @@ public class GBCPUImpl extends AbstractTimingSubject implements GBCPU {
         return 12;
     }
 
-    private void loadRegisterFromAddress(SingleRegister r1) {
-        registerManager.set(r1, getImmediateAddressValue());
-        numCyclesPassed += 16;
+    /**
+     * Pushes d's values onto the stack
+     *
+     * @return num of cpu cycles taken to perform op
+     * */
+    private int pushRegisterToStack(DoubleRegister d) {
+        pushToStack(registerManager.getHigh(d));
+        pushToStack(registerManager.getLow(d));
+
+        return 16;
     }
 
-    // loads the value in r1 to immediate address val
-    private void loadAddressFromRegister(SingleRegister r1) {
-        mmu.writeData(getImmediateAddressValue(), registerManager.get(r1));
-        numCyclesPassed += 16;
-    }
-
-    // r1 = high bits of r2
-    private void loadRegisterFromRegister(SingleRegister r1, DoubleRegister r2) {
-        registerManager.set(r1, registerManager.getHigh(r2));
-        numCyclesPassed += 8;
-    }
-
-    // r1.high = r2
-    private void loadRegisterFromRegister(DoubleRegister r1, SingleRegister r2) {
-        registerManager.set(r1, registerManager.get(r2));
-        numCyclesPassed += 8;
-    }
-
-    // r1.high = immediate value
-    private void loadRegisterFromImmediate8(DoubleRegister r1) {
-        registerManager.setHigh(r1, mmu.readData(++PC));
-        numCyclesPassed += 12;
-    }
-
-    private void loadRegisterFromImmediate16(DoubleRegister r1) {
-        registerManager.set(r1, getImmediateWord());
-    }
-
-    // special loads
-    private void loadAC() {
-        registerManager.set(SingleRegister.A,
-                mmu.readData(LOAD_SPECIAL_ADDRESS + registerManager.get(SingleRegister.C)));
-        numCyclesPassed += 8;
-    }
-
-    private void loadCA() {
-        mmu.writeData(LOAD_SPECIAL_ADDRESS + registerManager.get(SingleRegister.C),
-                registerManager.get(SingleRegister.A));
-        numCyclesPassed += 8;
-    }
-
-    private void LoadAHLD() {
-        int HLAddress = registerManager.get(DoubleRegister.HL);
-        registerManager.set(SingleRegister.A, mmu.readData(HLAddress));
-        registerManager.set(DoubleRegister.HL, --HLAddress);
-        numCyclesPassed += 8;
-    }
-
-    private void loadHLDA() {
-        int HLAddress = registerManager.get(DoubleRegister.HL);
-        mmu.writeData(HLAddress, registerManager.get(SingleRegister.A));
-        registerManager.set(DoubleRegister.HL, --HLAddress);
-        numCyclesPassed += 8;
-    }
-
-    private void loadAHLI() {
-        int HLAddress = registerManager.get(DoubleRegister.HL);
-        registerManager.set(SingleRegister.A, mmu.readData(HLAddress));
-        registerManager.set(DoubleRegister.HL, ++HLAddress);
-        numCyclesPassed += 8;
-    }
-
-    private void loadHLIA() {
-        int HLAddress = registerManager.get(DoubleRegister.HL);
-        mmu.writeData(HLAddress, registerManager.get(SingleRegister.A));
-        registerManager.set(DoubleRegister.HL, ++HLAddress);
-        numCyclesPassed += 8;
-    }
-
-    private void loadnA() {
-        mmu.writeData(LOAD_SPECIAL_ADDRESS + getImmediateByte(),
-                registerManager.get(SingleRegister.A));
-        numCyclesPassed += 12;
-    }
-
-    private void loadAn() {
-        registerManager.set(SingleRegister.A,
-                mmu.readData(LOAD_SPECIAL_ADDRESS + getImmediateByte()));
-        numCyclesPassed += 12;
-    }
-
-    private void loadSPHL() {
-        SP = registerManager.get(DoubleRegister.HL);
-        numCyclesPassed += 8;
-    }
-
-    private void loadHLSPn() {
-        int immediateValue8 = getImmediateByte();
-        registerManager.setZeroFlag(false);
-        registerManager.setOperationFlag(false);
-        registerManager.setHalfCarryFlag(BitUtils.isHalfCarryAdd(SP, immediateValue8));
-        registerManager.setCarryFlag(BitUtils.isCarryAdd(SP, immediateValue8));
-
-        registerManager.set(DoubleRegister.HL, (SP + immediateValue8) & 0xFFFF);
-
-        numCyclesPassed += 12;
-    }
-
-    private void loadnnSP() {
-        mmu.writeData(getImmediateAddressValue(), SP);
-        numCyclesPassed += 20;
-    }
-
-    private void push(DoubleRegister r1) {
-        --SP;
-        mmu.writeData(SP, registerManager.getHigh(r1));
-        --SP;
-        mmu.writeData(SP, registerManager.getLow(r1));
-        numCyclesPassed += 16;
-    }
-
-    private void pop(DoubleRegister r1) {
-        registerManager.setHigh(r1, mmu.readData(SP));
-        ++SP;
-        registerManager.setLow(r1, mmu.readData(SP));
-        ++SP;
-        numCyclesPassed += 12;
-    }
-
-
-    /*
-    *
-    * ALU METHODS
-    *
-    * */
-    private void add(SingleRegister r1, boolean addCarry) {
-        int r1Val = registerManager.get(r1);
-
-        doAdd(r1Val, addCarry);
-
-        numCyclesPassed += 4;
-    }
-
-    private void addImmediate(boolean addCarry) {
-        int immediateVal = getImmediateByte();
-
-        doAdd(immediateVal, addCarry);
-
-        numCyclesPassed += 8;
-    }
-
-    private void add(DoubleRegister r1, boolean addCarry) {
-        int memoryVal = mmu.readData(registerManager.get(r1));
-
-        doAdd(memoryVal, addCarry);
-
-        numCyclesPassed += 8;
-    }
-
-    private void doAdd(int val, boolean addCarry) {
-        int regAVal = registerManager.get(SingleRegister.A);
-
-        setAddFlags(regAVal, val);
-
-        int finalVal = regAVal + val;
-        if (addCarry) finalVal += registerManager.getCarryFlag() ? 1 : 0;
-
-        registerManager.set(SingleRegister.A, finalVal & 0xFF);
-    }
-
-    private void sub(SingleRegister r1, boolean subCarry) {
-        int r1Val = registerManager.get(r1);
-
-        doSub(r1Val, subCarry);
-
-        numCyclesPassed += 4;
-    }
-
-    private void subImmediate(boolean subCarry) {
-        int immediateVal = getImmediateByte();
-
-        doSub(immediateVal, subCarry);
-
-        numCyclesPassed += 8;
-    }
-
-    private void sub(DoubleRegister r1, boolean subCarry) {
-        int memoryVal = mmu.readData(registerManager.get(r1));
-
-        doSub(memoryVal, subCarry);
-
-        numCyclesPassed += 8;
-    }
-
-    private void doSub(int val, boolean subCarry) {
-        int regAVal = registerManager.get(SingleRegister.A);
-
-        setSubFlags(regAVal, val);
-
-        int finalVal = regAVal - val;
-        if (subCarry) finalVal -= registerManager.getCarryFlag() ? 1 : 0;
-
-        registerManager.set(SingleRegister.A, finalVal & 0xFF);
-    }
-
-
-    // set flags on doing sub op
-    private void setSubFlags(int regAVal, int otherVal) {
-        registerManager.setZeroFlag(BitUtils.isZeroSub(regAVal, otherVal));
-        registerManager.setOperationFlag(true);
-        registerManager.setHalfCarryFlag(BitUtils.isHalfCarrySub(regAVal, otherVal));
-        registerManager.setCarryFlag(BitUtils.isCarrySub(regAVal, otherVal));
-    }
-
-
-
-    // sets flags on doing an add op
-    private void setAddFlags(int regAVal, int otherVal) {
-        registerManager.setZeroFlag(BitUtils.isZeroAdd(regAVal, otherVal));
-        registerManager.setOperationFlag(false);
-        registerManager.setHalfCarryFlag(BitUtils.isHalfCarryAdd(regAVal, otherVal));
-        registerManager.setCarryFlag(BitUtils.isCarryAdd(regAVal, otherVal));
-    }
-
-
-    private void and(SingleRegister r1) {
-        int r1Val = registerManager.get(r1);
-
-        doAnd(r1Val);
-
-        numCyclesPassed += 4;
-    }
-
-    private void and() {
-        int immediateVal = getImmediateByte();
-
-        doAnd(immediateVal);
-
-        numCyclesPassed += 8;
-    }
-
-    private void and(DoubleRegister r1) {
-        int memoryVal = mmu.readData(registerManager.get(r1));
-
-        doAnd(memoryVal);
-
-        numCyclesPassed += 4;
-    }
-
-    private void doAnd(int otherVal) {
-        int regAVal = registerManager.get(SingleRegister.A);
-        int result = (regAVal & otherVal) & 0xFF;
-
-        registerManager.setZeroFlag(result == 0);
-        registerManager.setOperationFlag(false);
-        registerManager.setHalfCarryFlag(true);
-        registerManager.setCarryFlag(false);
-
-        registerManager.set(SingleRegister.A, result);
-    }
-
-    private void or(SingleRegister r1) {
-        int r1Val = registerManager.get(r1);
-
-        doOr(r1Val);
-
-        numCyclesPassed += 4;
-    }
-
-    private void or(DoubleRegister r1) {
-        int memoryVal = mmu.readData(registerManager.get(r1));
-
-        doOr(memoryVal);
-
-        numCyclesPassed += 8;
-    }
-
-    private void or() {
-        int immediateVal = getImmediateByte();
-
-        doOr(immediateVal);
-
-        numCyclesPassed += 8;
-    }
-
-    private void doOr(int otherVal) {
-        int regAVal = registerManager.get(SingleRegister.A);
-        int result = (regAVal | otherVal) & 0xFF;
-
-        registerManager.setZeroFlag(result == 0);
-        registerManager.setOperationFlag(false);
-        registerManager.setHalfCarryFlag(false);
-        registerManager.setCarryFlag(false);
-
-        registerManager.set(SingleRegister.A, result);
-    }
-
-    private void xor(SingleRegister r1) {
-        int r1Val = registerManager.get(r1);
-
-        doXor(r1Val);
-
-        numCyclesPassed += 4;
-    }
-
-    private void xor(DoubleRegister r1) {
-        int memoryVal = mmu.readData(registerManager.get(r1));
-
-        doXor(memoryVal);
-
-        numCyclesPassed += 8;
-    }
-
-    private void xor() {
-        int immediateVal = getImmediateByte();
-
-        doXor(immediateVal);
-
-        numCyclesPassed += 8;
-    }
-
-    private void doXor(int otherVal) {
-        int regAVal = registerManager.get(SingleRegister.A);
-        int result = (regAVal ^ otherVal) & 0xFF;
-
-        registerManager.setZeroFlag(result == 0);
-        registerManager.setOperationFlag(false);
-        registerManager.setHalfCarryFlag(false);
-        registerManager.setCarryFlag(false);
-
-        registerManager.set(SingleRegister.A, result);
-    }
-
-    private void cp(SingleRegister r1) {
-        int r1Val = registerManager.get(r1);
-
-        doCp(r1Val);
-
-        numCyclesPassed += 4;
-    }
-
-    private void cp(DoubleRegister r1) {
-        int memoryVal = mmu.readData(registerManager.get(r1));
-
-        doCp(memoryVal);
-
-        numCyclesPassed += 8;
-    }
-
-    private void cp() {
-        int immediateVal = getImmediateByte();
-
-        doCp(immediateVal);
-
-        numCyclesPassed += 8;
-    }
-
-    private void doCp(int otherVal) {
-        int regAVal = registerManager.get(SingleRegister.A);
-        int result = (regAVal - otherVal) & 0xFF;
-
-        registerManager.setZeroFlag(result == 0);
-        registerManager.setOperationFlag(true);
-        registerManager.setHalfCarryFlag(BitUtils.isHalfCarrySub(regAVal, otherVal));
-        registerManager.setCarryFlag(BitUtils.isCarrySub(regAVal, otherVal));
-    }
-
-    private void inc(SingleRegister r1) {
-        int r1Val = registerManager.get(r1);
-        int result = (r1Val + 1) & 0xFF;
-
-        registerManager.setZeroFlag(result == 0);
-        registerManager.setOperationFlag(false);
-        registerManager.setHalfCarryFlag(BitUtils.isHalfCarryAdd(r1Val, 1));
-
-        registerManager.set(r1, result);
-
-        numCyclesPassed += 4;
-    }
-
-    private void dec(SingleRegister r1) {
-        int r1Val = registerManager.get(r1);
-        int result = (r1Val - 1) & 0xFF;
-
-        registerManager.setZeroFlag(result == 0);
-        registerManager.setOperationFlag(true);
-        registerManager.setHalfCarryFlag(BitUtils.isHalfCarrySub(r1Val, 1));
-
-        registerManager.set(r1, result);
-
-        numCyclesPassed += 4;
-    }
-
-    private int getImmediateAddressValue() {
-        int address = getImmediateWord();
-        return mmu.readData(address);
+    /**
+     * Pops stack word into d
+     *
+     * @return num of cpu cycles taken to perform op
+     * */
+    private int popStackIntoRegister(DoubleRegister d) {
+        registerManager.setLow(d, popStack());
+        registerManager.setHigh(d, popStack());
+
+        return 12;
     }
 
     /**
      * Reads and returns the next 8 immediate bits from memory
      * */
     private int getImmediateByte() {
-        return mmu.readData(++PC);
+        return mmu.readData(++programCounter);
     }
 
     /**
      * Reads and returns the next 16 immediate bits from memory
      * */
     private int getImmediateWord() {
-        int val1 = mmu.readData(++PC);
-        int val2 = mmu.readData(++PC);
+        int val1 = mmu.readData(++programCounter);
+        int val2 = mmu.readData(++programCounter);
         return val2 << 8 | val1;
+    }
+
+    /**
+     * Pushes data on to the stack, decrements the stack pointer
+     * */
+    private void pushToStack(int data) {
+        int stackPointer = registerManager.get(DoubleRegister.SP);
+        mmu.writeData(stackPointer - 1, data);
+        registerManager.set(DoubleRegister.SP, stackPointer - 1);
+    }
+
+    /**
+     * Pops an element of the stack, increments the stack pointer
+     *
+     * @return element popped
+     * */
+    private int popStack() {
+        int stackPointer = registerManager.get(DoubleRegister.SP);
+        registerManager.set(DoubleRegister.SP, stackPointer + 1);
+        return mmu.readData(stackPointer);
     }
 }
