e0058c7e565918a5132b82b770eb7ad7305efc01
Siddharth Malik <siddharthmalik123@gmail.com>
2017-12-23 20:19:05 +0530

    added code coverage plugin

---
+++ src/main/java/MainModule.java
@@ -0,0 +1,11 @@
+import com.google.inject.AbstractModule;
+
+/**
+ * This is the module that has all the real implementations
+ * */
+public class MainModule extends AbstractModule {
+    @Override
+    protected void configure() {
+
+    }
+}
